15cc7500a0a81410cda27f8a7c8545d44691ed61
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-07 20:34:45 +1000

    Tweak examples

--- examples/gh-controller.js
+++ examples/gh-controller.js
@@ -2,7 +2,7 @@ var easyusb = require('..');
 var device = easyusb('0x12ba:0x0100');
 
 
-device.read(64, function(err, data) {
+device.read(function(err, data) {
   if (err) {
     console.error(err);
     return device.close();
--- examples/skyportal.js
+++ examples/skyportal.js
@@ -1,14 +1,15 @@
 var easyusb = require('..');
-var devices = [
+var device = easyusb([
   [ 0x1430, 0x1f17 ],
   [ 0x1430, 0x0150 ]
-];
+]);
 
-easyusb(devices)
-  .read(0x20, function(err, data) {
-    if (err) {
-      return console.error(err);
-    }
+device.read(0x20, function(err, data) {
+  if (err) {
+    console.error(err);
+    return device.close();
+  }
 
-    console.log('read data: ', data);
-  });
+  console.log('read data: ', data);
+  device.close();
+});
